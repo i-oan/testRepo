1ddee9d04cbe0ce8557620ec090eb7d69202a19e
i-oan <143788602+i-oan@users.noreply.github.com>
2023-09-13 14:37:24 +0000

    hello random person

--- hello.c
+++ hello.c
@@ -1,1 +1,27 @@
 //hello world program
+#include <stdio.h>
+
+char *names[10] = {
+    "Alice",
+    "Bob",
+    "Carol",
+    "Dave",
+    "Eve",
+    "Frank",
+    "Grace",
+    "Heidi",
+    "Ivan",
+    "Judy",
+};
+
+int main()
+{
+    char *name = names[rand() % 10];
+    printf("Hello %s!\n", name);
+    return 0;
+}
+
+
+
+
+
