fc95352d4556b39f12e4ae797cc76d4020a12de6
i-oan <143788602+i-oan@users.noreply.github.com>
2023-09-13 14:57:48 +0000

    removed the executable

--- hello.c
+++ hello.c
@@ -14,14 +14,68 @@ char *names[10] = {
     "Judy",
 };
 
+void hello(char *name);
+char* sayHello();
+
+//function that returns system time in seconds
+int get_time()
+{
+    return time(NULL);
+}
+
 int main()
 {
-    char *name = names[rand() % 10];
-    printf("Hello %s!\n", name);
+    //call sayHello function a thouseand times and count how many times each name is printed
+    srand(get_time());
+    int count[10] = {0};
+    int max = 0;
+    for (int i = 0; i < 1000; i++)
+    {
+        char *name = sayHello();
+        for (int j = 0; j < 10; j++)
+        {
+            if (name == names[j])
+            {
+                count[j]++;
+                if (count[j] > max)
+                {
+                    max = count[j];
+                }   
+
+            }
+        }
+    }
+    //print the results
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%s: %d\n", names[i], count[i]);
+    }
+    //print who is the luckiest
+    printf("The luckiest is: ");
+    for (int i = 0; i < 10; i++)
+    {
+        if (count[i] == max)
+        {
+            printf("%s ", names[i]);
+        }
+    }
+    printf("\n");
+
     return 0;
+}
+
+char* sayHello()
+{
+    
+    char *name = names[rand() % 10];
+    //hello(name);
+    return name;
 }
 
 
 
-
-
+///function that takes a name as param and prints hello name
+void hello(char *name)
+{
+    printf("Hello %s!\n", name);
+}
